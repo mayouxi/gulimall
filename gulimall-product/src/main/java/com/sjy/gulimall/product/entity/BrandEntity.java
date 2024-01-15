da486a3b2af613abc6a9fdf4d5c86b2dc6034f44
mayouxi <2785631446@qq.com>
2024-01-16 01:02:09 +0800

    新增错误码与统一异常捕获

--- gulimall-product/src/main/java/com/sjy/gulimall/product/entity/BrandEntity.java
+++ gulimall-product/src/main/java/com/sjy/gulimall/product/entity/BrandEntity.java
@@ -2,10 +2,15 @@ package com.sjy.gulimall.product.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-
-import java.io.Serializable;
-import java.util.Date;
 import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 
 /**
  * Ʒ?
@@ -20,33 +25,40 @@ public class BrandEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * Ʒ??id
+	 * 品牌id
 	 */
 	@TableId
 	private Long brandId;
 	/**
-	 * Ʒ????
+	 * 品牌名
 	 */
+	@NotBlank(message = "品牌名必须提交")
 	private String name;
 	/**
-	 * Ʒ??logo??ַ
+	 * 品牌logo地址
 	 */
+	@NotEmpty
+	@URL(message = "logo必须是一个合法的url地址")
 	private String logo;
 	/**
-	 * ???
+	 * 介绍
 	 */
 	private String descript;
 	/**
-	 * ??ʾ״̬[0-????ʾ??1-??ʾ]
+	 * 显示状态[0-不显示；1-显示]
 	 */
 	private Integer showStatus;
 	/**
-	 * ????????ĸ
+	 * 检索首字母
 	 */
+	@NotEmpty
+	@Pattern(regexp = "/^[a-zA-Z]$/", message = "检索首字母必须是一个字母")
 	private String firstLetter;
 	/**
-	 * ???
+	 * 排序
 	 */
+	@NotNull
+	@Min(value = 0, message = "排序必须大于等于0")
 	private Integer sort;
 
 }
