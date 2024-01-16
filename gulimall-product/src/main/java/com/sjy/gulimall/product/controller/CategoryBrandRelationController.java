a4ce7d68de284114ac2d560d7554eaaeae7892ae
mayouxi <2785631446@qq.com>
2024-01-16 15:43:34 +0800

    获取分类关联的品牌

--- gulimall-product/src/main/java/com/sjy/gulimall/product/controller/CategoryBrandRelationController.java
+++ gulimall-product/src/main/java/com/sjy/gulimall/product/controller/CategoryBrandRelationController.java
@@ -3,15 +3,17 @@ package com.sjy.gulimall.product.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.sjy.common.utils.PageUtils;
 import com.sjy.common.utils.R;
+import com.sjy.gulimall.product.entity.BrandEntity;
 import com.sjy.gulimall.product.entity.CategoryBrandRelationEntity;
 import com.sjy.gulimall.product.service.CategoryBrandRelationService;
+import com.sjy.gulimall.product.vo.BrandVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-
+import java.util.stream.Collectors;
 
 
 /**
@@ -90,5 +92,28 @@ public class CategoryBrandRelationController {
 
         return R.ok();
     }
+
+    /**
+     * 获取当前分类关联的所有品牌
+     * 1、 Controller: 处理请求，接受和校验数据
+     * 2、Service接受controller传来的数据，进行业务处理
+     * 3、Controller接受Service处理完的数据，封装成页面指定的vo
+     */
+    @GetMapping("/brands/list")
+    public R relationBrandsList(@RequestParam(value = "catId", required = true) Long catId){
+        List<BrandEntity> vos = categoryBrandRelationService.getBrandsByCatId(catId);
+
+        List<BrandVo> collect = vos.stream().map(item -> {
+            BrandVo brandVo = new BrandVo();
+            brandVo.setBrandId(item.getBrandId());
+            brandVo.setBrandName(item.getName());
+
+            return brandVo;
+        }).collect(Collectors.toList());
+
+        return R.ok().put("data", collect);
+    }
+
+
 
 }
--- gulimall-product/src/main/java/com/sjy/gulimall/product/service/impl/CategoryBrandRelationServiceImpl.java
+++ gulimall-product/src/main/java/com/sjy/gulimall/product/service/impl/CategoryBrandRelationServiceImpl.java
@@ -4,9 +4,14 @@ import com.sjy.gulimall.product.dao.BrandDao;
 import com.sjy.gulimall.product.dao.CategoryDao;
 import com.sjy.gulimall.product.entity.BrandEntity;
 import com.sjy.gulimall.product.entity.CategoryEntity;
+import com.sjy.gulimall.product.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -61,6 +66,20 @@ public class CategoryBrandRelationServiceImpl extends ServiceImpl<CategoryBrandR
     @Override
     public void updateCategory(Long catId, String name) {
         this.baseMapper.updateCategroy(catId, name);
+    }
+
+    @Autowired
+    BrandService brandService;
+
+    @Override
+    public List<BrandEntity> getBrandsByCatId(Long catId) {
+        List<CategoryBrandRelationEntity> catelogId = this.baseMapper.selectList(new QueryWrapper<CategoryBrandRelationEntity>().eq("catelog_id", catId));
+        List<BrandEntity> collect = catelogId.stream().map(item -> {
+            Long brandId = item.getBrandId();
+            BrandEntity byId = brandService.getById(brandId);
+            return byId;
+        }).collect(Collectors.toList());
+        return collect;
     }
 
 
