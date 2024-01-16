df4ed4ec2df920e627ccddeb2bb2b7f12c8c1f58
mayouxi <2785631446@qq.com>
2024-01-16 22:37:52 +0800

    gateway增加ware路由; 开启事务支持

--- gulimall-ware/src/main/java/com/sjy/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/sjy/gulimall/ware/GulimallWareApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan("com.sjy.gulimall.ware.dao")
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableTransactionManagement
 public class GulimallWareApplication {
     public static void main(String[] args) {
         SpringApplication.run(GulimallWareApplication.class, args);
