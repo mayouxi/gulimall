08b9f20aa4f384aee4dc5c9ed6c3b84148f18e36
mayouxi <2785631446@qq.com>
2024-01-17 00:18:20 +0800

    spu的查询与修改

--- gulimall-product/src/main/java/com/sjy/gulimall/product/controller/AttrController.java
+++ gulimall-product/src/main/java/com/sjy/gulimall/product/controller/AttrController.java
@@ -3,13 +3,16 @@ package com.sjy.gulimall.product.controller;
 import com.sjy.common.utils.PageUtils;
 import com.sjy.common.utils.R;
 import com.sjy.gulimall.product.entity.AttrEntity;
+import com.sjy.gulimall.product.entity.ProductAttrValueEntity;
 import com.sjy.gulimall.product.service.AttrService;
+import com.sjy.gulimall.product.service.ProductAttrValueService;
 import com.sjy.gulimall.product.vo.AttrRespVo;
 import com.sjy.gulimall.product.vo.AttrVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 
@@ -85,6 +88,25 @@ public class AttrController {
         PageUtils page = attrService.queryBaseAttrPage(params, catelogId, attrType);
         return R.ok().put("page", page);
     }
+
+    @Autowired
+    private ProductAttrValueService productAttrValueService;
+
+    @GetMapping("/base/listforspu/{spuId}")
+    public R baseListforspu(@PathVariable("spuId") Long spuId){
+        List<ProductAttrValueEntity> entityList = productAttrValueService.baseAttrlistForSpu(spuId);
+
+        return R.ok().put("data", entityList);
+    }
+
+    @PostMapping("/update/{spuId}")
+    public R updateSpuAttr(@PathVariable("spuId") Long spuId, @RequestBody List<ProductAttrValueEntity> entities){
+        productAttrValueService.updateSpuAttr(spuId, entities);
+
+        return R.ok();
+    }
+
+
 
 
 
