e046425e32da81cdba8f42daf66ba54584121700
mayouxi <2785631446@qq.com>
2024-01-20 16:24:37 +0800

    整合redis，并测试

--- gulimall-product/src/test/java/com/sjy/gulimall/product/GulimallProductApplicationTest.java
+++ gulimall-product/src/test/java/com/sjy/gulimall/product/GulimallProductApplicationTest.java
@@ -7,7 +7,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.UUID;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,6 +20,19 @@ public class GulimallProductApplicationTest {
     @Autowired
     BrandService brandService;
 
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+    @Test
+    public void testRedis() {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        // 保存
+        ops.set("hello", "world_" + UUID.randomUUID().toString());
+        // 查询
+        String hello = ops.get("hello");
+        System.out.println(hello);
+
+    }
     @Test
     public void contextLoads(){
         BrandEntity brandEntity = new BrandEntity();
